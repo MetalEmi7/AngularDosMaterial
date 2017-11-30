2ac888ac1dae610faa903adc19589ef472b1db31
Emiliano Alvarez <MetalEmi7@gmail.com>
2017-11-30 19:28:52 -0300

    1º Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Ejemplo de Material';
+   checked = false;
+  indeterminate = false;
+  align = 'start';
+  disabled = false;
+
+
+  foods = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {
+MatCheckboxModule,
+MatFormFieldModule,
+MatSelectModule,
+MatSliderModule,
+MatCardModule
+
+
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+/*
+comando
+npm install --save @angular/material @angular/cdk
+npm install --save @angular/animations
+
+
+
+*/
